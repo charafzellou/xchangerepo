54cc2d17612298b4553c39f3d3d7d3bcdc664185
Charaf ZELLOU <charaf.zellou@gmail.com>
2020-02-03 23:15:46 +0100

    Revision 2
    Revision 2

---
+++ api/CoinTicker.js
@@ -0,0 +1,17 @@
+const db = require('./db');
+const mongoose = require('mongoose');
+
+const CoinTickerSchema = mongoose.Schema({
+        coin_id : String,
+        coin_name: String,
+        currency: String,
+        currency_name: String,
+        time_stamp: String,
+        last_price: String,
+        price_24hr_pcnt: String,
+        volume_24hr: String,
+        vol_24hr_pcnt: String
+});
+
+const CoinTickerModel = db.model('CoinTicker', CoinTickerSchema);
+module.exports = CoinTickerModel; 
--- api/api_request.js
+++ api/api_request.js
@@ -1,4 +1,6 @@
 var request = require("request");
+var db = require("./db");
+var CoinTickerModel = require("./CoinTicker");
 
 const RapidAPIconfig = {
   method: 'GET',
@@ -11,14 +13,38 @@ const RapidAPIconfig = {
 };
 
 function RapidAPIrefine(result) {
-    delete result.success;
-    delete result.source;
-    delete result.utc_date;
+  delete result.success;
+  delete result.source;
+  delete result.utc_date;
+  return result;
+}
+
+function RapidAPIdefine(result) {
+  var newEntry = new CoinTickerModel(
+    {
+      coin_id : result.coin_id,
+      coin_name: result.coin_name,
+      currency: result.currency,
+      currency_name: result.currency_name,
+      time_stamp: result.time_stamp,
+      last_price: result.last_price,
+      price_24hr_pcnt: result.price_24hr_pcnt,
+      volume_24hr: result.volume_24hr,
+      vol_24hr_pcnt: result.vol_24hr_pcnt
+    }
+  );
+  return newEntry;
+}
+
+function RapidAPIsave(newEntry) {
+  newEntry.save(function (err, newEntry) {
+    if (err) return console.error(err);
+    newEntry.speak();
+  });
 }
 
 request(RapidAPIconfig, function (error, response, body) {
 	if (error) throw new Error(error);
   const result = JSON.parse(body);
-  RapidAPIrefine(result);
-  console.log(result);
+  RapidAPIsave(RapidAPIdefine(RapidAPIrefine(result)));  
 });
